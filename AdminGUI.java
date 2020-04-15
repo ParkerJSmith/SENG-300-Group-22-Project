b17d878bb3c73f4c8ca62b19d6dc8629cc698c5b
donaldoh <47364520+donaldoh@users.noreply.github.com>
2020-04-14 22:24:50 -0600

    Add files via upload

--- AdminGUI.java
+++ AdminGUI.java
@@ -22,11 +22,11 @@ import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class AdminGUI extends JPanel{
+public class AdminGUI extends JPanel{											/*The Admin's perspective*/
 
 	public AdminGUI(JFrame frame, SystemHandler systemHandler, User user) {
 
-		frame.setTitle("Admin");
+		frame.setTitle("Admin");												//Set Title//					
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -34,23 +34,23 @@ public class AdminGUI extends JPanel{
 		frame.getContentPane().add(this, BorderLayout.CENTER);
 		setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Admin");
+		JLabel lblNewLabel = new JLabel("Admin");								// "Admin" Label in top left//
 		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 20));
 		lblNewLabel.setBounds(33, 11, 91, 22);
 		add(lblNewLabel);
 
-		JLabel lblNewLabel_1 = new JLabel("Scholarship");
+		JLabel lblNewLabel_1 = new JLabel("Scholarship");						// Create label "Scholarship//
 		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 15));
 		lblNewLabel_1.setBounds(178, 61, 125, 22);
 		add(lblNewLabel_1);
 		
-		JLabel lblSignout = new JLabel("Sign out");
+		JLabel lblSignout = new JLabel("Sign out");								// Create Label to sign out//
 		lblSignout.setFont(new Font("Arial", Font.PLAIN, 15));
 		lblSignout.setBounds(350, 10, 110, 22);
 		lblSignout.setForeground(new Color(1, 112, 169));
 		lblSignout.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
+			public void mouseClicked(MouseEvent arg0) {							// If Sign out text is clicked, popup to confirm//
 				int n = JOptionPane.showConfirmDialog(
 					    frame,
 					    "Are you sure you want to sign out?",
@@ -74,10 +74,10 @@ public class AdminGUI extends JPanel{
 		});
 		add(lblSignout);
 
-		JButton btnNewButton = new JButton("View Scholarships");
+		JButton btnNewButton = new JButton("View Scholarships");				// Create Button, named "View scholarships"//
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
+			public void mouseClicked(MouseEvent arg0) {							// If this button is created, ViewScholarshipGUI window created//
 				ViewScholarshipGUI vs = new ViewScholarshipGUI(frame, systemHandler, user);
 				frame.setContentPane(vs);
 			}
@@ -85,10 +85,10 @@ public class AdminGUI extends JPanel{
 		btnNewButton.setBounds(154, 104, 120, 23);
 		add(btnNewButton);
 		
-		JButton seeApplications = new JButton("View Applications");
+		JButton seeApplications = new JButton("View Applications");				// Create Button, named "View Applications"//
 		seeApplications.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
+			public void mouseClicked(MouseEvent arg0) {							// If this button is created, ViewApplicationsGUI window created//
 				frame.setContentPane(new ViewApplicationsGUI(frame, systemHandler, user));
 			}
 		});
